0fa323d3761de7e6887259f5f3940763ade62326
swarnakini <swarnagowri.kini@gmail.com>
2025-03-19 12:05:16 +0530

    Initial commit - Simple Node.js App

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+// Define the server port
+const PORT = 3000;
+
+// Create a server
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello, Node.js Server without Express!");
+});
+
+// Start the server
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
