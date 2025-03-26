4c7c1038686e70b31b243a8a2c43134c3d68e25c
swarnakini <swarnagowri.kini@gmail.com>
2025-03-26 10:29:43 +0530

    Updated code

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = 3000;
 const server = http.createServer((req, res) => {
   if (req.method == "GET") {
     res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ message: "Hello" }));
+    res.end(JSON.stringify({ message: "Hello Code branch" }));
   } else {
     res.writeHead(404, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ error: "not found" }));
